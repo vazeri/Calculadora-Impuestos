ae5689a773a55af334581f0de5813ab972d1133b
Erick Vazquez <gzerox@gmail.com>
2018-01-08 23:12:37 -0600

    Fix

--- 01_Calculadora_IVA.js
+++ 01_Calculadora_IVA.js
@@ -1,37 +1,39 @@
-    /* Erick Vazquez
-    Ejercicio de Capitulo 1 del libro, You dont know JavaScript edición 2015 (Pag 26)
-    Calculadora para la compra de teleofonos por un usuario
-    Calculo de Impuestos y total a Pagar por el cliente */
-    const PrecioTelefono = 500;
-    const IVA = 0.16;
-    const Presupuesto = 250;
-    const Accesorios = 50;
+            /* Erick Vazquez
+            Ejercicio final del Capitulo 1 del libro, You dont know JavaScript edición 2015 (Pag 26)
+            Calculadora para la compra de telefonos por un usuario y su calculo de Impuestos */
+            
+            const PrecioTelefono = 99.99;
+            const IVA = 0.16;
+            const Presupuesto = 200;
+            const Accesorios = 9.99;
 
-    var nomina = 1000;
-    var cantidad= 0;
+            var nomina = 303.91
+            var cantidad = 0;
 
-    function calcImpuestos(cantidad) {
-        return PrecioTelefono * IVA;
-    }
+            function calcImpuestos(cantidad) {
+                return cantidad * IVA;
+            }
 
-    //Cantidad TOTAL  a imprimir en el ticket
-    function PrecioFinal(cantidad) {
-        return "$" + amount.toFixed( 2 );
-    }
+            //Cantidad TOTAL  a imprimir en el ticket
+            function PrecioFinal(cantidad) {
+                return "$" + cantidad.toFixed( 2 ) + "pesos";
+            }
 
-    //Seguir comprando telefonos mientras tengas dinero 
-    while (nomina < PrecioTelefono){
+            //Seguir comprando telefonos mientras tengas dinero 
+            while (cantidad < nomina){
 
-        cantidad = cantidad + PrecioTelefono;
+                cantidad = cantidad + PrecioTelefono;
 
-        if (cantidad <= Presupuesto) {
-            cantidad = cantidad + Accesorios;        
-        }
-    }
+                if (cantidad <= Presupuesto) {
+                    cantidad = cantidad + Accesorios;        
+                }
+            }
 
-    // Para el pago de impuestos 
-    //cantidad = cantidad + calcImpuestos(cantidad);
-    console.log("El Total a pagar es: " + PrecioFinal(cantidad));
-    if (amount > nomina) {
-        console.log("No te alcanza el saldo");
-    }
+            // Para el pago de impuestos 
+            cantidad = cantidad + calcImpuestos(cantidad);
+
+            console.log("El Total a pagar es: " +   (cantidad) );
+
+            if (cantidad > nomina) {
+                console.log("No te alcanza este telefono");
+            }
